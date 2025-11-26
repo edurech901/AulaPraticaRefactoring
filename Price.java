433f5f4e3bf19631d03a0e672114be51c6f1ab34
edurech901 <eduardo.rech901@gmail.com>
2025-11-25 23:37:13 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -21,4 +21,11 @@ public abstract class Price {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
